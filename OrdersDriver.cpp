8765eb4463f19168f701c18e382d8f65cbf45a12
domdoesbu <rhiannon@rhiannondoesburg.com>
2023-02-04 17:22:58 -0500

    good luck

--- OrdersDriver.cpp
+++ OrdersDriver.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include "Orders.cpp"
 #include "Orders.h"
 using namespace std;
 
-void fillVector(vector<Orders>&);
-//Fill the vector - fills in the orders
-
 int main(){
-
     Deploy d1("deploy");
     Advance a1("advance");
     Bomb b1("bomb");
@@ -17,13 +12,5 @@ int main(){
     Airlift ar1("airlift");
     Negotiate n1("negotiate");
 
-    vector<Orders> vectorOrder = {d1, a1, b1, bl1, ar1, n1};
-
-    for(int i = 0; i < vectorOrder.size(); i++){
-        vectorOrder[i].execute();
-    }
-}
-
-void fillVector(vector<Orders>& newOrder){
     
 }
