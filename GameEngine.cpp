d85a56dadd8b2b11796f2e88e26211c3d31b29e2
Daniel <57418761+D-grimut@users.noreply.github.com>
2023-02-19 12:20:48 -0500

    Game engine v1 (#3)
    * Game Engine base files
    
    TODO:
    -Finish the loop for the run
    -Finish the methods for the transition function
    -Make the Game Engine Driver
    -Add loops around map loaded, player added, issue orders, and execute orders
    
    * Changes to Engine
    
    -does all state transitions
    
    * Changes
    
    Made the int current state a pointer
    
    * Final changes
    
    -added all the constructors
    -commented out all of the code
    
    * Memory leaks fixed
    
    ---------
    
    Co-authored-by: Nicholas Pop <94801567+Cocovert1@users.noreply.github.com>

---
+++ GameEngine.cpp
@@ -0,0 +1,521 @@
+// GameEngine.cpp
+#include "GameEngine.h"
+
+/*
+This section is for all the constructors used for each class
+*/
+
+// Constructor for game engine
+GameEngine::GameEngine()
+{
+    current_state = new int(0); // default: every user starts at current state 0 aka. "Start"
+}
+
+// Destructor for game engine
+GameEngine::~GameEngine()
+{
+    delete current_state;
+    current_state = nullptr;
+}
+
+// Copy constructor for game engine
+GameEngine::GameEngine(const GameEngine &copy)
+{
+    current_state = new int(*(copy.current_state));
+}
+
+// Constructor for start state
+StartState::StartState(GameEngine *engine)
+{
+    this->engine = engine;
+}
+
+// Copy constructor for start state
+StartState::StartState(const StartState &copy)
+{
+    this->engine = new GameEngine(*copy.engine);
+}
+
+// Destructor for start state
+StartState::~StartState()
+{
+    delete engine;
+    engine = nullptr;
+}
+
+// Constructor for Map loaded state
+MapLoadedState::MapLoadedState(GameEngine *engine)
+{
+    this->engine = engine;
+}
+
+// Copy constructor for map loaded state
+MapLoadedState::MapLoadedState(const MapLoadedState &copy)
+{
+    this->engine = new GameEngine(*copy.engine);
+}
+
+// Destructor for map loaded state
+MapLoadedState::~MapLoadedState()
+{
+    delete engine;
+    engine = nullptr;
+}
+
+// Constructor for map validated state
+MapValidatedState::MapValidatedState(GameEngine *engine)
+{
+    this->engine = engine;
+}
+
+// Copy constructor for map validated state
+MapValidatedState::MapValidatedState(const MapValidatedState &copy)
+{
+    this->engine = new GameEngine(*copy.engine);
+}
+
+// Destructor for map validated state
+MapValidatedState::~MapValidatedState()
+{
+    delete engine;
+    engine = nullptr;
+}
+
+// Constructor for players added state
+PlayersAddedState::PlayersAddedState(GameEngine *engine)
+{
+    this->engine = engine;
+}
+
+// Copy constructor for players added state
+PlayersAddedState::PlayersAddedState(const PlayersAddedState &copy)
+{
+    this->engine = new GameEngine(*copy.engine);
+}
+
+// Destructor for players added state
+PlayersAddedState::~PlayersAddedState()
+{
+    delete engine;
+    engine = nullptr;
+}
+
+// Constructor assign reinforcement state
+AssignReinforcementState::AssignReinforcementState(GameEngine *engine)
+{
+    this->engine = engine;
+}
+
+// Copy consturctor for assign reinforcement state
+AssignReinforcementState::AssignReinforcementState(const AssignReinforcementState &copy)
+{
+    this->engine = new GameEngine(*copy.engine);
+}
+
+// Destructor for assingn reinforcement state
+AssignReinforcementState::~AssignReinforcementState()
+{
+    delete engine;
+    engine = nullptr;
+}
+
+// Constructor for issue order state
+IssueOrderState::IssueOrderState(GameEngine *engine)
+{
+    this->engine = engine;
+}
+
+// Copy constructor for issue order state
+IssueOrderState::IssueOrderState(const IssueOrderState &copy)
+{
+    this->engine = new GameEngine(*copy.engine);
+}
+
+// Destructor for issue order state
+IssueOrderState::~IssueOrderState()
+{
+    delete engine;
+    engine = nullptr;
+}
+
+// Constructor for execute order state
+ExecuteOrderState::ExecuteOrderState(GameEngine *engine)
+{
+    this->engine = engine;
+}
+
+// Copy constructor for execute order state
+ExecuteOrderState::ExecuteOrderState(const ExecuteOrderState &copy)
+{
+    this->engine = new GameEngine(*copy.engine);
+}
+
+// Destructor for execute order state
+ExecuteOrderState::~ExecuteOrderState()
+{
+    delete engine;
+    engine = nullptr;
+}
+
+// Construct for win state
+WinState::WinState(GameEngine *engine)
+{
+    this->engine = engine;
+}
+
+// Copy constructor for win state
+WinState::WinState(const WinState &copy)
+{
+    this->engine = new GameEngine(*copy.engine);
+}
+
+// Destructor for win state
+WinState::~WinState()
+{
+    delete engine;
+    engine = nullptr;
+}
+
+// Play method: this method runs a continuous loop and plays the game
+void GameEngine::Play()
+{
+    // create the states
+    GameEngine *Engine = new GameEngine();
+    StartState *start_state = new StartState(Engine);
+    MapLoadedState *map_loaded_state = new MapLoadedState(Engine);
+    MapValidatedState *map_validated_state = new MapValidatedState(Engine);
+    PlayersAddedState *players_added_state = new PlayersAddedState(Engine);
+    AssignReinforcementState *assign_reinforcement_state = new AssignReinforcementState(Engine);
+    IssueOrderState *issue_order_state = new IssueOrderState(Engine);
+    ExecuteOrderState *execute_order_state = new ExecuteOrderState(Engine);
+    WinState *win_state = new WinState(Engine);
+
+    // loop that keeps the game running, ends when the state is 8 aka. "End"
+    while (*Engine->getState() != 8)
+    {
+        Engine->Commands();
+        std::cout << "Your move: ";
+        std::string input;
+        std::cin >> input;
+        if (*Engine->getState() == 0)
+        {
+            start_state->StartInput(input);
+        }
+        else if (*Engine->getState() == 1)
+        {
+            map_loaded_state->MapLoadedInput(input);
+        }
+        else if (*Engine->getState() == 2)
+        {
+            map_validated_state->ValidateInput(input);
+        }
+        else if (*Engine->getState() == 3)
+        {
+            players_added_state->PlayersAddedInput(input);
+        }
+        else if (*Engine->getState() == 4)
+        {
+            assign_reinforcement_state->AssignReinforcementsInput(input);
+        }
+        else if (*Engine->getState() == 5)
+        {
+            issue_order_state->IssueOrderInput(input);
+        }
+        else if (*Engine->getState() == 6)
+        {
+            execute_order_state->ExecuteOrderInput(input);
+        }
+        else if (*Engine->getState() == 7)
+        {
+            win_state->WinInput(input);
+        }
+    }
+
+    Engine->~GameEngine();
+    start_state->~StartState();
+    map_loaded_state->~MapLoadedState();
+    map_validated_state->~MapValidatedState();
+    players_added_state->~PlayersAddedState();
+    assign_reinforcement_state->~AssignReinforcementState();
+    issue_order_state->~IssueOrderState();
+    execute_order_state->~ExecuteOrderState();
+    win_state->~WinState();
+}
+
+// Getter for the private attribute state
+int *GameEngine::getState()
+{
+    return this->current_state;
+}
+
+/*
+    This section downwards is for transition functions only
+    It is comprised of the base command function that is called by all functions.
+    and each states transition function that is used to transition from state to state.
+*/
+
+// Function that will change the state of the game
+void GameEngine::TransitionTo(int state)
+{
+    current_state = new int(state);
+}
+
+//  shows valid game engine commands for each state
+void GameEngine::Commands()
+{
+    std::cout << "Valid commands: ";
+    if (*current_state == 0)
+    {
+        std::cout << "\n1. loadmap" << std::endl;
+    }
+    else if (*current_state == 1)
+    {
+        std::cout << "\n1. loadmap" << std::endl;
+        std::cout << "2. validatemap" << std::endl;
+    }
+    else if (*current_state == 2)
+    {
+        std::cout << "\n1. addplayer" << std::endl;
+    }
+    else if (*current_state == 3)
+    {
+        std::cout << "\n1. addplayer" << std::endl;
+        std::cout << "2. assigncountries" << std::endl;
+    }
+    else if (*current_state == 4)
+    {
+        std::cout << "\n1. issueorder" << std::endl;
+    }
+    else if (*current_state == 5)
+    {
+        std::cout << "\n1. issueorder" << std::endl;
+        std::cout << "2. endissueorders" << std::endl;
+    }
+    else if (*current_state == 6)
+    {
+        std::cout << "\n1. win" << std::endl;
+        std::cout << "2. execorder" << std::endl;
+        std::cout << "3. endexecorders" << std::endl;
+    }
+    else if (*current_state == 7)
+    {
+        std::cout << "\n1. end" << std::endl;
+        std::cout << "2. play" << std::endl;
+    }
+}
+
+// Checks for the input in start state
+void StartState::StartInput(const std::string &input)
+{
+    if (input == "loadmap")
+    {
+        std::cout << "\nSuccess!\n"
+                  << std::endl;
+        engine->TransitionTo(1);
+    }
+    else
+    {
+        std::cout << "\nInvalid command\n"
+                  << std::endl;
+        engine->Commands();
+        std::cout << "Your move: ";
+        std::string temp;
+        std::cin >> temp;
+        StartInput(temp);
+    }
+}
+
+// Checks for the input in Map load state
+void MapLoadedState::MapLoadedInput(const std::string &input)
+{
+    if (input == "validatemap")
+    {
+        std::cout << "\nSuccess!\n"
+                  << std::endl;
+        engine->TransitionTo(2);
+    }
+    else if (input == "loadmap")
+    {
+        std::cout << "\nMap loaded\n"
+                  << std::endl;
+        engine->Commands();
+        std::cout << "Your move: ";
+        std::string temp;
+        std::cin >> temp;
+        MapLoadedInput(temp);
+    }
+    else
+    {
+        std::cout << "\nInvalid command\n"
+                  << std::endl;
+        engine->Commands();
+        std::cout << "Your move: ";
+        std::string temp;
+        std::cin >> temp;
+        MapLoadedInput(temp);
+    }
+}
+
+// Checks for the input in Map validate state
+void MapValidatedState::ValidateInput(const std::string &input)
+{
+    if (input == "addplayer")
+    {
+        std::cout << "\nSuccess!\n"
+                  << std::endl;
+        engine->TransitionTo(3);
+    }
+    else
+    {
+        std::cout << "\nInvalid command\n"
+                  << std::endl;
+        engine->Commands();
+        std::cout << "Your move: ";
+        std::string temp;
+        std::cin >> temp;
+        ValidateInput(temp);
+    }
+}
+
+// Checks for the input in Player Added state
+void PlayersAddedState::PlayersAddedInput(const std::string &input)
+{
+    if (input == "assigncountries")
+    {
+        std::cout << "\nSuccess!\n"
+                  << std::endl;
+        engine->TransitionTo(4);
+    }
+    else if (input == "addplayer")
+    {
+        std::cout << "\nPlayer added\n"
+                  << std::endl;
+        engine->Commands();
+        std::cout << "Your move: ";
+        std::string temp;
+        std::cin >> temp;
+        PlayersAddedInput(temp);
+    }
+    else
+    {
+        std::cout << "\nInvalid command\n"
+                  << std::endl;
+        engine->Commands();
+        std::cout << "Your move: ";
+        std::string temp;
+        std::cin >> temp;
+        PlayersAddedInput(temp);
+    }
+}
+
+void AssignReinforcementState::AssignReinforcementsInput(const std::string &input)
+{
+    if (input == "issueorder")
+    {
+        std::cout << "\nSuccess!\n"
+                  << std::endl;
+        engine->TransitionTo(5);
+    }
+    else
+    {
+        std::cout << "\nInvalid command\n"
+                  << std::endl;
+        engine->Commands();
+        std::cout << "Your move: ";
+        std::string temp;
+        std::cin >> temp;
+        AssignReinforcementsInput(temp);
+    }
+}
+
+void IssueOrderState::IssueOrderInput(const std::string &input)
+{
+    if (input == "endissueorders")
+    {
+        std::cout << "\nSuccess!\n"
+                  << std::endl;
+        engine->TransitionTo(6);
+    }
+    else if (input == "issueorder")
+    {
+        std::cout << "\nOrder issued\n"
+                  << std::endl;
+        engine->Commands();
+        std::cout << "Your move: ";
+        std::string temp;
+        std::cin >> temp;
+        IssueOrderInput(temp);
+    }
+    else
+    {
+        std::cout << "\nInvalid command\n"
+                  << std::endl;
+        engine->Commands();
+        std::cout << "Your move: ";
+        std::string temp;
+        std::cin >> temp;
+        IssueOrderInput(temp);
+    }
+}
+
+void ExecuteOrderState::ExecuteOrderInput(const std::string &input)
+{
+    if (input == "win")
+    {
+        std::cout << "\nSuccess!\n"
+                  << std::endl;
+        engine->TransitionTo(7);
+    }
+    else if (input == "execorder")
+    {
+        std::cout << "\nOrder executed\n"
+                  << std::endl;
+        engine->Commands();
+        std::cout << "Your move: ";
+        std::string temp;
+        std::cin >> temp;
+        ExecuteOrderInput(temp);
+    }
+    else if (input == "endexecorders")
+    {
+        std::cout << "\nSuccess! Returning to Assignment of reinforcements.\n"
+                  << std::endl;
+        engine->TransitionTo(4);
+    }
+    else
+    {
+        std::cout << "\nInvalid command\n"
+                  << std::endl;
+        engine->Commands();
+        std::cout << "Your move: ";
+        std::string temp;
+        std::cin >> temp;
+        ExecuteOrderInput(temp);
+    }
+}
+
+// TODO: finish the win input
+void WinState::WinInput(const std::string &input)
+{
+    if (input == "end")
+    {
+        engine->TransitionTo(8);
+        std::cout << "\nThank you for playing!" << std::endl;
+        exit(0);
+    }
+    else if (input == "play")
+    {
+        engine->TransitionTo(0);
+        std::cout << "\n Back to the start." << std::endl;
+    }
+    else
+    {
+        std::cout << "\nInvalid command\n"
+                  << std::endl;
+        engine->Commands();
+        std::cout << "Your move: ";
+        std::string temp;
+        std::cin >> temp;
+        WinInput(temp);
+    }
+}
---
+++ GameEngine.h
@@ -0,0 +1,130 @@
+#include <iostream>
+
+class GameEngine
+{
+public:
+    GameEngine();                       // default constructor
+    ~GameEngine();                      // destructor
+    GameEngine(const GameEngine &copy); // copy constructor
+    void Play();                        // general run method
+    void Commands();                    // print the valid commands
+    void TransitionTo(int state);       // transition function
+    int *getState();                    // getter
+
+private:
+    int *current_state; // current state : int or string?
+};
+
+/*
+Starting now, all of the classes will be seperate states. Each state will have
+-A constructor with parameter
+-A copy constructor
+-A destructor
+-An input method
+-A private attribute for engine
+*/
+
+// start state
+class StartState
+{
+public:
+    StartState(GameEngine *engine);
+    ~StartState();
+    StartState(const StartState &copy);
+    void StartInput(const std::string &input);
+
+private:
+    GameEngine *engine;
+};
+
+// map loaded state
+class MapLoadedState
+{
+public:
+    // constructor
+    MapLoadedState(GameEngine *engine);
+    ~MapLoadedState();
+    MapLoadedState(const MapLoadedState &copy);
+    void MapLoadedInput(const std::string &input);
+
+private:
+    GameEngine *engine;
+};
+
+// map validate state
+class MapValidatedState
+{
+public:
+    MapValidatedState(GameEngine *engine);
+    ~MapValidatedState();
+    MapValidatedState(const MapValidatedState &copy);
+    void ValidateInput(const std::string &input);
+
+private:
+    GameEngine *engine;
+};
+
+// players added state
+class PlayersAddedState
+{
+public:
+    PlayersAddedState(GameEngine *engine);
+    ~PlayersAddedState();
+    PlayersAddedState(const PlayersAddedState &copy);
+    void PlayersAddedInput(const std::string &input);
+
+private:
+    GameEngine *engine;
+};
+
+// assign reinforcement state
+class AssignReinforcementState
+{
+public:
+    AssignReinforcementState(GameEngine *engine);
+    ~AssignReinforcementState();
+    AssignReinforcementState(const AssignReinforcementState &copy);
+    void AssignReinforcementsInput(const std::string &input);
+
+private:
+    GameEngine *engine;
+};
+
+// issue order state
+class IssueOrderState
+{
+public:
+    IssueOrderState(GameEngine *engine);
+    ~IssueOrderState();
+    IssueOrderState(const IssueOrderState &copy);
+    void IssueOrderInput(const std::string &input);
+
+private:
+    GameEngine *engine;
+};
+
+// exec orders state
+class ExecuteOrderState
+{
+public:
+    ExecuteOrderState(GameEngine *engine);
+    ~ExecuteOrderState();
+    ExecuteOrderState(const ExecuteOrderState &copy);
+    void ExecuteOrderInput(const std::string &input);
+
+private:
+    GameEngine *engine;
+};
+
+// win state
+class WinState
+{
+public:
+    WinState(GameEngine *engine);
+    ~WinState();
+    WinState(const WinState &copy);
+    void WinInput(const std::string &input);
+
+private:
+    GameEngine *engine;
+};
